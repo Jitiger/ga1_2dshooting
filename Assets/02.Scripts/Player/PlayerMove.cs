8e0f349555458faf3f5c72a1e8b06342bc6e046d
Jitiger <100251888+Jitiger@users.noreply.github.com>
2026-09-02 11:12:24 +0900

    fix: 플레이어 대각선 이동 정규화

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -27,7 +27,8 @@ public class PlayerMove : MonoBehaviour
         // 속도 = 방향 * 속력                        // 매직 넘버란: 보는 사람에 따라 의미가 달라질 수 있는
         // 헷갈리는 숫자
         // 0.06
-        transform.Translate(direction * Speed * Time.deltaTime);
+        Vector2 normalizedSpeed = (direction * Speed).normalized; // 벡터의 길이를 1로 만들어 주는 것(즉, 방향만 유지한다.)
+        transform.Translate(direction * normalizedSpeed * Time.deltaTime);
         // deltaTime: 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 MS로 반환
         
         // 새로운 위치 = 현재 위치 + (방향 * 속력 * 시간)
