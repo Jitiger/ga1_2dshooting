a76095f1d0ab2fd0a5e1892f69cf1a4c4d80cd4c
Jitiger <100251888+Jitiger@users.noreply.github.com>
2026-09-02 14:27:20 +0900

    feat: 총알 발사(생성과 이동)

---
+++ Assets/02.Scripts/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // 목적: 총알을 위로 움직이고 싶다.
+    public float Speed = 0;
+    private void Update()
+    {
+        Vector2 direction = Vector2.up; // new Vector2(1,0)
+        transform.Translate(direction * Speed * Time.deltaTime);
+    }
+
+}
