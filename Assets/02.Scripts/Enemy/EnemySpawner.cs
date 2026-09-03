aa3965ac6d2a34dd3cb6d880461b8a0b2ec00dc4
Jitiger <100251888+Jitiger@users.noreply.github.com>
2026-09-03 18:13:35 +0900

    feat: 적 스폰 기능 테스트

--- Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
+    public GameObject[] EnemyPrefabs;
+
     public float SpawnTime = 2f;
     public float SpawnTimer = 0;
+
     public float SpawnMaxPositionX;
     public float SpawnMinPositionX;
     public float SpawnPositionY;
-    public float MoveSpeed = 3f;
-
-    private void Start()
-    {
-    }
 
     private void Update()
     {
+        SpawnTimer -= Time.deltaTime;
+
         EnemySpawner();
     }
 
@@ -25,10 +25,11 @@ public class EnemySpawn : MonoBehaviour
             float randomX = Random.Range(SpawnMinPositionX, SpawnMaxPositionX);
             Vector2 spawnPosition = new Vector2(randomX, SpawnPositionY);
 
-            //여기에 적 생성 코드 넣어야함 ㅠ ㅠ
+            int randomEnemy = Random.Range(0, EnemyPrefabs.Length);
 
-            Vector2 direction = Vector2.down;
-            transform.Translate(direction * MoveSpeed * Time.deltaTime);
+            GameObject enemy = Instantiate(EnemyPrefabs[randomEnemy]);
+            enemy.transform.position = spawnPosition;
+
             SpawnTimer = SpawnTime;
         }
     }
