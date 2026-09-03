3f2ab2a1cfcdce2daafdcb69146848d4b2b75fbd
Jitiger <100251888+Jitiger@users.noreply.github.com>
2026-09-03 10:44:32 +0900

    feat: 코딩컨벤션 기능 추가

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -6,27 +6,30 @@ public class PlayerFire : MonoBehaviour
     // 필요 속성
     // - 총알 프리팹
     public GameObject GreenBullet;
+
     // - 생성 위치(총구)
     public Transform RightFirePoint;
     public Transform LeftFirePoint;
-    
+
     public GameObject RedBullet;
     public Transform SubRightFirePoint;
+
     public Transform SubLeftFirePoint;
+
     // - 쿨타이머
     public float CoolTime = 0.5f;
     public float CoolTimer = 0;
 
     // - Auto 모드
     public bool AutoFireMode = false;
-    
+
     private void Start()
     {
         CoolTimer = CoolTime;
     }
 
     // public Transform[] FirePoint;
-    
+
     private void Update()
     {
         // 오토 공격 모드 토글
@@ -36,15 +39,17 @@ public class PlayerFire : MonoBehaviour
         }
 
         CoolTimer -= Time.deltaTime;
-        
+
         // 1. 쿨타이머가 0초 이하이고 && (스페이스바를 누르거나 || 오토 모드라면)
         if (CoolTimer <= 0 && (Input.GetKeyDown(KeyCode.Space) || AutoFireMode))
         {
             // 2. 발사
+
+
             Fire();
             SubFire();
             // 3. 쿨타이머 초기화
-            CoolTimer=CoolTime;
+            CoolTimer = CoolTime;
         }
     }
 
@@ -59,20 +64,19 @@ public class PlayerFire : MonoBehaviour
             SubLeftbullet.transform.position = SubLeftFirePoint.position;
         }
     }
+
     public void Fire()
     {
         // 1. 스페이스바를 누르면
-       // if (Input.GetKeyDown(KeyCode.Space))
+        // if (Input.GetKeyDown(KeyCode.Space))
         {
             // 2. 총알 프리팹을 생성한다.
             // Instantiate는 프리팹을 복사해서 (Monobehaviour를 상속받는) 게임 오브젝트를 생성하고 씬에 넣어주는 기능
             GameObject Rightbullet = Instantiate((GreenBullet));
             Rightbullet.transform.position = RightFirePoint.position; // 생성한 총알의 위치를 나(플레이어)의 위치로!!
-            
+
             GameObject Leftbullet = Instantiate((GreenBullet));
             Leftbullet.transform.position = LeftFirePoint.position;
-
         }
-        
     }
 }
--- Assets/02.Scripts/Scripts/Bullet.cs
+++ Assets/02.Scripts/Scripts/Bullet.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public enum BulletType
+    {
+        main,
+        sub
+    }
+
+    public BulletType Type;
+
     // 목적: 총알을 위로 움직이고 싶다.
     public float Speed;
-    
+
     private void Update()
     {
         Vector2 direction = Vector2.up; // new Vector2(1,0)
         transform.Translate(direction * Speed * Time.deltaTime);
     }
-    
 }
