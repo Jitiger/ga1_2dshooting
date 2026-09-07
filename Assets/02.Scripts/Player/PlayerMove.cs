1bc68774c84ac6a1956475cd32ff537ef66ef33f
Jitiger <100251888+Jitiger@users.noreply.github.com>
2026-09-07 15:53:13 +0900

    design: 플레이어 이동 애니메이션 추가

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -1,10 +1,12 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     // 목적: 키보드 입력에 따라서 플레이어 이동 처리를 하고 싶다.
-
     // 필요 필드:
+    private Animator _animator;
     [Header("플레이어 이동 속도")]
     [SerializeField] private float _speed;
 
@@ -15,6 +17,11 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float _minPositionX;
     // 매 프레임마다 실행된다.
     // 초당 프레임 실행 횟수는: 별다른 설정이 없을 경우 가능한 많이
+    //객체가 생성될 때 한 번 실행된다.
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
     private void Update()
     {
         Move();
@@ -27,6 +34,7 @@ public class PlayerMove : MonoBehaviour
 
         // 2. 키보드 입력에 따라 방향을 구한다.
         Vector2 normalizedDirection = new Vector2(h, v).normalized;
+        _animator.SetInteger("x", (int)normalizedDirection.x);
 
         // 3. 방향과 속력에 따라 이동한다.
         Vector2 newPosition = transform.position + (Vector3)normalizedDirection * _speed * Time.deltaTime;
